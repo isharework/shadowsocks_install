0efaad8b077c0c0c0277f80409c24c58ca64d8e4
isharework <35474565+isharework@users.noreply.github.com>
2018-01-19 09:38:43 +0800

    update download url
    update download url

--- shadowsocksR.sh
+++ shadowsocksR.sh
@@ -20,7 +20,7 @@ echo "#############################################################"
 echo
 
 libsodium_file="libsodium-1.0.16"
-libsodium_url="https://github.com/jedisct1/libsodium/releases/download/1.0.16/libsodium-1.0.16.tar.gz"
+libsodium_url="https://codeload.github.com/isharework/libsodium/tar.gz/1.0.16"
 
 #Current folder
 cur_dir=`pwd`
